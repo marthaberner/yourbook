5ed78deac7d254f6ae1287e533bba6a886411925
marthaberner <marthaberner.mb@gmail.com>
2018-04-09 20:00:03 -0600

    scaffold stories

---
+++ db/migrate/20180410015919_create_stories.rb
@@ -0,0 +1,11 @@
+class CreateStories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :stories do |t|
+      t.string :author
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
